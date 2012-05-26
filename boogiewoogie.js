b23041a896c74149369b3995149ee84a86555d54
John J. Workman <johnjworkman@gmail.com>
2012-05-26 15:06:17 -0400

    Breaking out demo.js and getting canvas element

--- boogiewoogie.js
+++ boogiewoogie.js
@@ -1,3 +1,12 @@
-var BoogieWoogie = function(opts) {};
+var BoogieWoogie = function(opts) {
+    var defaults = {
+        canvasId: 'program'
+    };
+    
+    var canvas = document.getElementById(opts.canvasId || defaults.canvasId);
+    if (!canvas) throw "Could not get canvas element.";
+    
+    console.log(canvas);
+};
 
 BoogieWoogie.prototype = {};
