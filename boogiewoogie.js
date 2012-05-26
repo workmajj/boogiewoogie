ee3beba4ad3f8286482854cb7cfcc7da930a31dc
John J. Workman <johnjworkman@gmail.com>
2012-05-26 15:37:45 -0400

    Making a jQ-like extend function for opts parsing

--- boogiewoogie.js
+++ boogiewoogie.js
@@ -3,10 +3,24 @@ var BoogieWoogie = function(opts) {
         canvasId: 'source'
     };
     
-    var canvas = document.getElementById(opts.canvasId || defaults.canvasId);
-    if (!canvas) throw new Error("Could not get canvas element with source");
+    this.opts = extend(defaults, opts);
+    
+    var canvas = document.getElementById(this.opts.canvasId);
+    if (!canvas) throw new Error("Could not get canvas element");
     
     console.log(canvas);
 };
 
-BoogieWoogie.prototype = {};
+BoogieWoogie.prototype = {
+    opts: null
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+function extend(orig, delta) {
+    var obj = JSON.parse(JSON.stringify(orig)); // copy
+    for (var i in delta) {
+        if (delta.hasOwnProperty(i)) obj[i] = delta[i];
+    }
+    return obj;
+}
