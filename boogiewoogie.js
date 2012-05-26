6777ab327ed3b8fb979ca9d608369e3dff1cd727
John J. Workman <johnjworkman@gmail.com>
2012-05-26 15:21:15 -0400

    Restructuring demo files/directories

--- boogiewoogie.js
+++ boogiewoogie.js
@@ -1,10 +1,10 @@
 var BoogieWoogie = function(opts) {
     var defaults = {
-        canvasId: 'program'
+        canvasId: 'source'
     };
     
     var canvas = document.getElementById(opts.canvasId || defaults.canvasId);
-    if (!canvas) throw "Could not get canvas element.";
+    if (!canvas) throw new Error("Could not get canvas element with source");
     
     console.log(canvas);
 };
