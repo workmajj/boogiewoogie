1f2a1d781b075d03e16c92695c63a2f0c5a220b2
John J. Workman <johnjworkman@gmail.com>
2012-05-26 16:24:22 -0400

    Setting up canvas element and loading source image

--- boogiewoogie.js
+++ boogiewoogie.js
@@ -1,14 +1,22 @@
 var BoogieWoogie = function(opts) {
     var defaults = {
-        canvasId: 'source'
+        file: 'source.png',
+        canvasId: 'canvas'
     };
-    
+
     this.opts = extend(defaults, opts);
-    
+
     var canvas = document.getElementById(this.opts.canvasId);
     if (!canvas) throw new Error("Could not get canvas element");
-    
-    console.log(canvas);
+    var ctx = canvas.getContext('2d');
+
+    var img = new Image();
+    img.src = this.opts.file;
+    img.onload = function() {
+        canvas.setAttribute('width', img.width);
+        canvas.setAttribute('height', img.height);
+        ctx.drawImage(img, 0, 0);
+    };
 };
 
 BoogieWoogie.prototype = {
