c774c9114f1f4f6b8708fb71d178c60f9cd62750
John J. Workman <johnjworkman@gmail.com>
2012-05-26 22:15:23 -0400

    Using base64-encoded image to skirt same origin policy for canvas data

--- boogiewoogie.js
+++ boogiewoogie.js
@@ -10,29 +10,38 @@ function extend(orig, delta) {
 
 var BoogieWoogie = function(opts) {
     var defaults = {
-        canvasId: 'canvas',
-        imgFile: 'source.png'
+        imgSrc: null,
+        imgType: null,
+        canvasId: 'program'
     };
 
     this.opts = extend(defaults, opts);
 
-    this._load(this.opts.canvasId, this.opts.imgFile);
+    if (!this.opts.imgSrc || !this.opts.imgType) {
+        throw new Error("No image source or image type set");
+    }
+
+    this.init();
 };
 
 BoogieWoogie.prototype = {
     opts: null,
 
-    _load: function(canvasId, imgFile) {
-        var canvas = document.getElementById(canvasId);
+    init: function() {
+        var canvas = document.getElementById(this.opts.canvasId);
         if (!canvas) throw new Error("Could not get canvas element");
         var ctx = canvas.getContext('2d');
 
         var img = new Image();
-        img.src = imgFile;
+        img.src = 'data:image/' + this.opts.imgType + ';base64,' +
+            this.opts.imgSrc; // source is base64-encoded
         img.onload = function() {
             canvas.setAttribute('width', img.width);
             canvas.setAttribute('height', img.height);
             ctx.drawImage(img, 0, 0);
+
+            var d = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            console.log(d.data);
         };
     }
 };
--- demo/demo.js
+++ demo/demo.js
@@ -2,9 +2,20 @@ $(function() {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+// hello world (http://www.dangermouse.net/esoteric/piet/samples.html)
+var src = 'iVBORw0KGgoAAAANSUhEUgAAAB4AAAAdCAIAAAAyxktbAAAABmJLR0QA/wD/AP+gva' +
+    'eTAAAACXBIWXMAAAsSAAALEgHS3X78AAAAB3RJTUUH1AMUBzUEfbNeqwAAAO9JREFUeJzVlV' +
+    '0OhCAMhKcb70WPXk42+4ARQSir4MNOSFRKv1bKDwj4zQCC12bJ0m8bOhIyvVB6Qwb6+ObHXA' +
+    'DCdi+H3GHQQdYzambd/JOL52hAkbVUzzlltABcSv+cuSjoi9CJGC3HmA9x7IyjozI7rjDrml' +
+    'Uv6NuptgKoRlRZ3+VabGetEBQrJN4EOyEVBmY0Q3FwzVQxgHA2uhkR7Rk6+W0iPf8I6jN00t' +
+    'TxpOrNWvcqAPbpplnbqEozp/YemgA6XCfkoRfP6xfR7lz/oBByJWO5OWfRZ6kWnyvR1T29BG' +
+    '07ueztlpEWJgP+5+L7AlSybJ19aBENAAAAAElFTkSuQmCC';
+var type = 'png';
+
 var broadway = new BoogieWoogie({
-    canvasId: 'canvas',
-    imgFile: 'source.png'
+    imgSrc: src,
+    imgType: type,
+    canvasId: 'program'
 });
 
 ////////////////////////////////////////////////////////////////////////////////
