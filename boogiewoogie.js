a7bb2cb68b9134040f71fe6d70de5c66b4f2118b
John J. Workman <johnjworkman@gmail.com>
2012-05-28 22:00:39 -0400

    Reorganizing prototype

--- boogiewoogie.js
+++ boogiewoogie.js
@@ -19,42 +19,46 @@ var BoogieWoogie = function(cfg) {
         throw new Error('No image source or image type set');
     }
 
+    this.stack = []; // TODO: move this to run/init once built
+
     this._load();
 };
 
 BoogieWoogie.prototype = {
     cfg: null,
+    stack: null,
+    val: null // current color block value
+};
 
-    _load: function() {
-        var canvas = document.getElementById(this.cfg.canvasId);
-        if (!canvas) throw new Error('Could not get canvas element');
-        var ctx = canvas.getContext('2d');
+BoogieWoogie.prototype._load = function() {
+    var canvas = document.getElementById(this.cfg.canvasId);
+    if (!canvas) throw new Error('Could not get canvas element');
+    var ctx = canvas.getContext('2d');
 
-        var img = new Image();
-        img.src = 'data:image/' + this.cfg.imgType + ';base64,' +
-            this.cfg.imgSrc; // source is base64-encoded
+    var img = new Image();
+    img.src = 'data:image/' + this.cfg.imgType + ';base64,' +
+        this.cfg.imgSrc; // source is base64-encoded
 
-        var that = this;
-        img.onload = function() {
-            canvas.setAttribute('width', img.width);
-            canvas.setAttribute('height', img.height);
+    var that = this;
+    img.onload = function() {
+        canvas.setAttribute('width', img.width);
+        canvas.setAttribute('height', img.height);
 
-            ctx.drawImage(img, 0, 0);
+        ctx.drawImage(img, 0, 0);
 
-            var d = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            that._canvasToPiet(d);
-        };
-    },
+        var d = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        that._canvasToPiet(d);
+    };
+};
 
-    _canvasToPiet: function(d) {
-        for (var i = 0, l = d.data.length; i < l; i += 4) {
-            var r = d.data[i];
-            var g = d.data[i + 1];
-            var b = d.data[i + 2];
-            var a = d.data[i + 3];
+BoogieWoogie.prototype._canvasToPiet = function(d) {
+    for (var i = 0, l = d.data.length; i < l; i += 4) {
+        var r = d.data[i];
+        var g = d.data[i + 1];
+        var b = d.data[i + 2];
+        var a = d.data[i + 3];
 
-            console.log('i=' + i, r, g, b, a);
-        }
+        console.log('i=' + i, r, g, b, a);
     }
 };
 
