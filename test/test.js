3d933ad0d5fe09552270a079c55d66c104e0eecf
John J. Workman <johnjworkman@gmail.com>
2012-05-30 16:32:52 -0400

    Stubbing out tests file to get a handle on QUnit

--- test/test.js
+++ test/test.js
@@ -4,6 +4,22 @@ $(function() {
 
 var broadway = new BoogieWoogie();
 
+module('foo');
+
+test('bar', function() {
+    expect(1);
+
+    equal(true, true, 'baz');
+});
+
+test('baz', function() {
+    expect(3);
+
+    equal(true, true, 'qux');
+    equal(true, true, 'quux');
+    equal(true, true, 'lol');
+});
+
 ////////////////////////////////////////////////////////////////////////////////
 
 });
